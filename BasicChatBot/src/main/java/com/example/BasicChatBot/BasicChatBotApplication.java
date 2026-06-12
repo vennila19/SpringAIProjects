bcd8087bdff79128675abe155c7579cf2e5705bc
Vennila Natrajan <57398585+vennila19@users.noreply.github.com>
2026-06-12 18:31:33 +0000

    Add ChatBotController

--- BasicChatBot/src/main/java/com/example/BasicChatBot/BasicChatBotApplication.java
+++ BasicChatBot/src/main/java/com/example/BasicChatBot/BasicChatBotApplication.java
@@ -1,13 +1,25 @@
 package com.example.BasicChatBot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+
 public class BasicChatBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicChatBotApplication.class, args);
 	}
 
+	 @GetMapping("/chat")
+    public String chat(@RequestParam String question) {
+        return "Welcome to Chatbot" + question;
+    }
+
+	
 }
