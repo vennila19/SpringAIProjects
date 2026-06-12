fdb8df310bf35fb74fe1b5c4855da2fab920fb78
Vennila Natrajan <57398585+vennila19@users.noreply.github.com>
2026-06-12 19:08:25 +0530

    Add files via upload

---
+++ BasicChatBot/src/main/java/com/example/BasicChatBot/BasicChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.BasicChatBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicChatBotApplication.class, args);
+	}
+
+}
