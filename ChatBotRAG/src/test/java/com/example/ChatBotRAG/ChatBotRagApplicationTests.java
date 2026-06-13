8f2c6eeecb53289bbdb60ad2f512c4f1525497cf
Vennila Natrajan <57398585+vennila19@users.noreply.github.com>
2026-06-13 23:20:49 +0530

    Add files via upload

---
+++ ChatBotRAG/src/test/java/com/example/ChatBotRAG/ChatBotRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChatBotRAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
