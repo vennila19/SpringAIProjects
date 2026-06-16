b60bddfe41cec7c06369f90c609c563ac6e43e00
Vennila Natrajan <57398585+vennila19@users.noreply.github.com>
2026-06-16 06:16:11 +0000

    add

--- ChatBotRAG/src/main/java/com/example/ChatBotRAG/ChatBotRagApplication.java
+++ ChatBotRAG/src/main/java/com/example/ChatBotRAG/ChatBotRagApplication.java
@@ -1,4 +1,4 @@
-package com.example.ChatBotRAG;
+package com.example.chatBotRAG;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
